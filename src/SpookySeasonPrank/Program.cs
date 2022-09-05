29aa584c4e3f53c5215080de8b80a219c1838791
dev <dev@example.com>
2022-09-05 16:00:57 +0100

    initial commit

---
+++ src/SpookySeasonPrank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true, reloadOnChange: false)
+    .Build();
+
+var twilioAccountSid = config["Twilio:AccountSid"];
+var twilioAuthToken = config["Twilio:AuthToken"];
+
+TwilioClient.Init(twilioAccountSid, twilioAuthToken);
+
+CallResource.Create(
+    url: new Uri("https://{Your bucket name}.s3.amazonaws.com/output.mp3"),
+    method: Twilio.Http.HttpMethod.Get, 
+    
+    to: new Twilio.Types.PhoneNumber("{Your victim’s phone number}"),
+    from: new Twilio.Types.PhoneNumber("{Your Twilio phone number}")
+);
---
+++ src/SpookySeasonPrankPrepareAudio/Program.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Polly;
+using Amazon.Polly.Model;
+
+var pollyClient = new AmazonPollyClient();
+var startSpeechSynthesisTaskRequest = new StartSpeechSynthesisTaskRequest
+{
+    Text = "James! I know what you did last summer!. You're not getting away with it this time!",
+    VoiceId = VoiceId.Matthew,
+    OutputFormat = OutputFormat.Mp3,
+    OutputS3BucketName = "my-halloween-recordings1",
+    OutputS3KeyPrefix = "polly-output",
+};
+await pollyClient.StartSpeechSynthesisTaskAsync(startSpeechSynthesisTaskRequest);
